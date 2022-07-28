d26e4e8165b60cbed3ea9d78819675c54e32c254
“Sushree” <sushreesureleesahu98@gmail.com>
2022-07-29 00:50:12 +0530

    [Sushree | Added contact details in Address book]

---
+++ AddressBookSystem/PersonInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    class PersonInfo
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int zip { get; set; }
+        private int _mobileNum { get; set; }
+        public string emailID { get; set; }
+        public int MobileNum
+        {
+            get
+            {
+                return _mobileNum;
+            }
+            set
+            {
+                if (value < 10)
+                {
+                    Console.WriteLine("Enter mobile number consist of 10 digits.");
+                    return;
+                }
+                else
+                {
+                    _mobileNum = value;
+                }
+            }
+        }
+    }
+}
